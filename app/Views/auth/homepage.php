998a2ffda2c1e80fa51f702c881af2e5baecc81a
Trevor Waweru <trevor.k.x.waweru@gmail.com>
2023-06-19 00:11:49 +0300

    Authentication Module 3

--- app/Views/auth/homepage.php
+++ app/Views/auth/homepage.php
@@ -3,7 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Homepage</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link href="<?php echo base_url('css/homepage.css'); ?>" rel="stylesheet">
+  <script type="text/javascript" src="<?php echo base_url('js/main.js');?>"></script>
 </head>
 <body>
   <h1>This is the Homepage</h1>
@@ -16,5 +20,9 @@
 
     echo "Welcome $fullName";
   ?>
+  <br>
+  <img src="<?php echo base_url('images/user1.jpg');?>" alt="User profile picture">
+
+
 </body>
 </html>
