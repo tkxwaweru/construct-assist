b26dda6b829e0cb0c8e1146b708cb63e6d7008cc
Swansong101 <owensnicole03@gmail.com>
2023-07-12 18:09:17 +0300

    3 links, 2 routes and a link in href to professionals

--- app/Views/professional-dashboards/manage-prof-profile.php
+++ app/Views/professional-dashboards/manage-prof-profile.php
@@ -23,9 +23,9 @@
     <nav>
       <div class="side_navbar">
         <a  href="#">Home</a>
-        <a  class="active" href="#">Manage Profile</a>
-        <a  class="ratings" href="#">View Ratings</a>
-        <a  class="submit-verifications" href="#">Submit Verification Documentation</a>
+        <a  class="active" href="<?php echo site_url('professionalProfile'); ?>">Manage Profile</a>
+        <a   href="#">View Ratings</a>
+        <a   href="#">Submit Verification Documentation</a>
         <a class="log-out-button" href="<?php echo site_url('logout'); ?>">Logout</a>
       </div>
     </nav>
