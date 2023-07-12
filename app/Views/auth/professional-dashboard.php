832fc6c51a30ac3ca19488a05688ad7f4a42a8c9
Swansong101 <owensnicole03@gmail.com>
2023-07-12 16:41:13 +0300

    Professional dashboard navigation

--- app/Views/auth/professional-dashboard.php
+++ app/Views/auth/professional-dashboard.php
@@ -28,7 +28,7 @@
     <nav>
       <div class="side_navbar">
         <a class="active" href="#">Home</a>
-        <a  href="<?php echo base_url('manageProfile'); ?>">Manage Profile</a>
+        <a  href="<?php echo site_url('professionalProfile'); ?>">Manage Profile</a>
         <a  href="#">View Ratings</a>
         <a  href="#">Submit Verification Documentation</a>
         <a class="log-out-button" href="<?php echo site_url('logout'); ?>">Logout</a>
